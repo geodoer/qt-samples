120436aa8425bcbb083de946cc14d6db44f23ba3
geodoer <geodoer@outlook.com>
2022-03-30 23:14:33 +0800

    feat: jsonio and xmlio
    Signed-off-by: geodoer <geodoer@outlook.com>

---
+++ simple/JsonIO/QJsonAndQMap.cpp
@@ -0,0 +1,69 @@
+﻿//https://blog.csdn.net/chenyijun/article/details/103032017
+#include <QMap>
+#include <QVariant>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+#include <QDebug>
+
+void QMapToJson()
+{
+	//===map
+    QMap<QString, QVariant> mapData = QMap<QString, QVariant>();
+    mapData.insert("id", "3000101");
+    mapData.insert("number", "15289705326");
+    mapData.insert("name", "Job Bresh");
+
+	//===map to json
+    QJsonDocument doc = QJsonDocument::fromVariant(QVariant(mapData));
+
+    QByteArray jsonByteArray = doc.toJson();
+    QString jsonString = QString(jsonByteArray);
+	qDebug() << "QMap<QString, QVariant> to json: \n" << jsonString;
+ 
+    QByteArray njba = jsonString.toUtf8();
+    QJsonObject nobj = QJsonObject(QJsonDocument::fromJson(njba).object());
+
+    //字符串转换后的对象
+	qDebug() << "string conversion of object: \n"
+             << nobj.take("id").toString() << endl
+             << nobj.take("number").toString() << endl
+             << nobj.take("name").toString() << endl;
+}
+ 
+void JsonToQMap()
+{
+    //===json
+    QJsonObject json;
+    json.insert("id", "3000102");
+    json.insert("number", "15789705324");
+    json.insert("name", "Job Victor");
+ 
+    QJsonDocument document;
+    document.setObject(json);
+    QByteArray byteArray = document.toJson(QJsonDocument::Compact);
+    QString strJson(byteArray);
+	qDebug() << strJson;
+
+    //==Json to QMap
+    QVariantMap map = json.toVariantMap();
+    int numMap = map.size();
+    //取值
+	qDebug() << "numMap===========" << numMap;
+	qDebug() << "map[\"id\"]===========" << map["id"];
+	qDebug() << "map[\"id\"].toString()===========" << map["id"].toString();
+    //qDebug() << "map.take(\"number\")===========" << map.take("number").toString();
+	qDebug() << "map.value(\"name\")===========" << map.value("name").toString();
+
+	for(auto it = map.constBegin(); it!=map.constEnd(); ++it)
+	{
+		qDebug() << "key:" << it.key() << "  value:" << it.value();
+	}
+}
+
+int main()
+{
+	QMapToJson();
+	JsonToQMap();
+	return 0;
+}
---
+++ simple/XmlIO/QDomDocumentSample.cpp
@@ -0,0 +1,102 @@
+﻿#include<QDomDocument>
+
+#include<QTextStream>
+#include<QFile>
+
+#include<iostream>
+
+void Write(const QString& xmlFp)
+{
+	QDomDocument doc;
+
+	QDomElement menu = doc.createElement("RibbonMenu");
+	doc.appendChild(menu);
+
+	for(auto i :
+		{
+			1,2
+		})
+	{
+		auto page = doc.createElement("RibbonPage");
+		page.setAttribute("title", "Page" + QString::number(i));
+
+		for(QString j :
+			{ 'A','B','C'
+			})
+		{
+			auto group = doc.createElement("RibbonGroup");
+			group.setAttribute("title", "Group+" + j);
+
+			for(QString z :
+				{ "undo", "redo"
+				})
+			{
+				QDomElement command = doc.createElement("Command");
+				command.setAttribute("title", z + "_t");
+				command.setAttribute("command", z);
+				command.setAttribute("icon", z + ".png");
+				group.appendChild(command);
+			}
+
+			page.appendChild(group);
+		}
+
+		menu.appendChild(page);
+	}
+
+	QFile fileIn(xmlFp);
+
+	if(!fileIn.open(QIODevice::WriteOnly | QIODevice::Text))
+	{
+		return;
+	}
+
+	QTextStream in(&fileIn);
+	doc.save(in, 4);
+	fileIn.close();
+}
+
+void Read(const QString& xmlFp)
+{
+	QDomDocument doc;
+	QFile file(xmlFp);
+
+	if(!file.open(QIODevice::ReadOnly))
+	{
+		return;
+	}
+
+	if(!doc.setContent(&file))
+	{
+		file.close();
+		return;
+	}
+
+	file.close();
+
+	//RibbonMenu
+	QDomElement docElem = doc.documentElement();
+	std::cout << qPrintable(docElem.tagName()) << std::endl;
+	
+	for(QDomNode n = docElem.firstChild(); !n.isNull(); n = n.nextSibling())
+	{
+		QDomElement e = n.toElement();
+
+		if(e.isNull())
+		{
+			continue;
+		}
+
+		std::cout << qPrintable(e.tagName()) << " " << qPrintable(e.attribute("title")) << std::endl; // the node really is an element.
+	}
+}
+
+int main()
+{
+	QString xmlFp = "menu.xml";
+
+	Write(xmlFp);
+	Read(xmlFp);
+
+	return 0;
+}
